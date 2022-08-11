4a6cfa122acb54c4260eb1de2bb443f1729cb9c6
Rizky Kurniawan Ritonga <rizkykurniawanritonga@gmail.com>
2022-08-11 11:05:34 +0700

    final ye ye ye

---
+++ components/BubbleChat.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="rounded-full last:rounded-bl-none transition-all bg-gray-200 w-fit leading-tight md:py-4 py-2 md:px-8 px-4 mb-1 md:text-2xl text-sm antialiased font-sans">
+    <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: "Chat",
+  props: {
+    show: Boolean,
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,32 @@
 <template>
   <div ref="message" class="apppesan md:m-4 m-2">
+
     <Head>
       <Meta property="og:site_name" content="rizkykr.com" />
       <Meta property="og:title" content="RizkyKR" />
       <Meta property="og:description" content="I design and code things on the web." />
       <Meta property="og:type" content="website" />
       <Meta property="og:url" content="https://www.rizkykr.com" />
-      <Meta property="og:image" content="img/og-image.png" />
+      <Meta property="og:image" content="/img/og-image.png" />
       <Meta property="og:image:type" content="image/png" />
       <Meta property="og:image:width" content="768" />
       <Meta property="og:image:height" content="416" />
     </Head>
-    <Chat v-for="txt in pesan" v-html="txt"></Chat>
+    <BubbleChat v-for="txt in pesan" v-html="txt" />
   </div>
 </template>
 <script>
-import Chat from "~~/components/Chat.vue";
 export default defineComponent({
   async setup() {
+    function hanyaCode(txt) {
+      return txt.split(",")[2].replace('}', '').split(":")[1].split('"').join('').toLocaleLowerCase()
+    }
     const { geoUrl } = useRuntimeConfig();
-    const [{ data: fetchLokasi }] = await Promise.all([
-      useFetch(`https://api.myip.com`),
-      // useFetch(`${geoUrl}/`),
-      // useFetch(`https://api.github.com/orgs/nuxt/repos`)
+    const [{ data: lokasi }] = await Promise.all([
+      useFetch(`/`, { parseResponse: txt => hanyaCode(txt), baseURL: geoUrl }),
     ])
 
-    return { fetchLokasi }
+    return { lokasi }
   },
   head: {
     title: "RizkyKR - Web Fullstack Developer",
@@ -39,7 +40,7 @@ export default defineComponent({
       loadingText:
         '<span class="flex md:gap-2 gap-1 md:py-2 py-1"><span class="flex h-3 w-3 relative"><span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-400 opacity-75"></span><span class="relative inline-flex rounded-full h-3 w-3 bg-gray-500"></span></span><span class="flex h-3 w-3 relative"><span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-400 opacity-75"></span><span class="relative inline-flex rounded-full h-3 w-3 bg-gray-500"></span></span><span class="flex h-3 w-3 relative"><span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-400 opacity-75"></span><span class="relative inline-flex rounded-full h-3 w-3 bg-gray-500"></span></span></span>',
       messages: {
-        ID: [
+        id: [
           "Halo, Assalamualaikum 👋",
           "Saya Rizky",
           "Saya mendesain dan membuat kode apapun di web.",
@@ -48,7 +49,7 @@ export default defineComponent({
           this.getCurTime(),
           "👀 R."
         ],
-        EN: [
+        en: [
           "Hey there 👋",
           "I'm Rizky",
           "I design and code things on the web",
@@ -60,42 +61,28 @@ export default defineComponent({
       },
     };
   },
-  watch: {
-    // whenever question changes, this function will run
-    loc(dt) {
-      this.kirimPesan(dt);
-    }
-  },
   methods: {
-    kirimPesan(lg) {
-      const datapesan = this.messages[(lg=='ID'?lg:'EN')];
+    kirimPesan() {
+      const lg = this.lokasi;
+      const datapesan = this.messages[(lg == 'id' ? lg : 'en')];
       if (this.pesancur < datapesan.length) {
         this.pesan.push(this.loadingText);
         setTimeout(() => {
           this.pesan[this.pesancur] = datapesan[this.pesancur];
           this.pesancur = this.pesancur + 1;
           setTimeout(() => {
-            this.kirimPesan(this.loc);
+            this.kirimPesan();
           }, 1000);
         }, datapesan[this.pesancur].replace(/<(?:.|\n)*?>/gm, "").length * this.typingSpeed + 500);
       }
     },
     getCurTime() {
-      var date = new Date();
-      var hours = date.getHours();
-      var minutes = date.getMinutes();
-      var current = hours + minutes * 0.01;
-      // if (current >= 5 && current < 19) return "Have a nice day";
-      // if (current >= 19 && current < 22) return "Have a nice evening";
-      // if (current >= 5 && current < 22) return "Have a nice day";
-      // if (current >= 22 || current < 5) return "Have a good night";
+      var date = new Date(), current = date.getHours() + date.getMinutes() * 0.01;
       return (current >= 5 && current < 22) ? "Have a nice day" : "Have a good night";
     },
   },
   mounted() {
-    this.loc = JSON.parse(this.fetchLokasi).cc;
-    console.log(this.loc);
+    this.kirimPesan()
   },
-  components: { Chat },
 });
 </script>
