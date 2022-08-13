456ddaf2156c2dd7e12c67afed0ca9b6832a3274
Rizky Kurniawan Ritonga <rizkykurniawanritonga@gmail.com>
2022-08-14 06:39:45 +0700

    recolor dialog

--- components/DialogSosmed.vue
+++ components/DialogSosmed.vue
@@ -21,14 +21,14 @@
         class="flex items-center justify-center min-h-full p-4 text-center sm:p-0"
       >
         <div
-          class="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full delay-100"
+          class="relative bg-white dark:bg-slate-800 rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full delay-100"
           :class="
             open
               ? 'opacity-100 translate-y-0 sm:scale-100 ease-out duration-300'
               : 'opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95 ease-in duration-200'
           "
         >
-          <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div class="bg-white dark:bg-slate-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
             <div class="sm:flex sm:items-start">
               <div
                 class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-sky-100 sm:mx-0 sm:h-10 sm:w-10"
@@ -48,17 +48,17 @@
               </div>
               <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                 <h3
-                  class="text-lg leading-6 font-medium text-gray-900"
+                  class="text-lg leading-6 font-medium text-gray-900 dark:text-white"
                   id="modal-title"
                 >
                   {{ lang.ks[lokasi] }}
                 </h3>
                 <div class="mt-2">
-                  <p class="text-sm text-gray-500">
+                  <p class="text-sm text-gray-500 dark:text-white">
                     {{ lang.ucan[lokasi] }}
                   </p>
                 </div>
-                <div class="mt-4 flex gap-3 justify-center">
+                <div class="mt-4 flex md:flex-row flex-col gap-3 justify-center">
                   <a
                     v-for="dt in sosmed"
                     :href="dt[1]"
@@ -71,11 +71,11 @@
               </div>
             </div>
           </div>
-          <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <div class="bg-gray-50 dark:bg-slate-900 px-4 py-4 sm:px-6 sm:flex sm:flex-row-reverse">
             <button
               type="button"
               @click="tutupmodal"
-              class="mt-3 w-full inline-flex justify-center rounded-full border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              class="w-full inline-flex justify-center rounded-full border border-gray-300 shadow-sm px-4 py-2 bg-white dark:bg-slate-800 text-base font-medium text-gray-700 dark:text-white hover:bg-slate-50 dark:hover:bg-slate-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:ml-3 sm:w-auto sm:text-sm"
             >
               {{ lang.btn.close[lokasi] }}
             </button>
