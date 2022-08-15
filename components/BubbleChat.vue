5f83e3e3645021bf985d9559bba11cb1022ab3b6
Rizky Kurniawan Ritonga <rizkykurniawanritonga@gmail.com>
2022-08-16 05:59:12 +0700

    animasi santuy

--- components/BubbleChat.vue
+++ components/BubbleChat.vue
@@ -1,6 +1,6 @@
 <template>
   <span
-    class="relative animate__animated animate__faster rounded-full last-of-type:rounded-bl-none transition-all bg-white dark:bg-slate-800 text-slate-900 dark:text-white ring-1 ring-slate-900/5 shadow w-fit !leading-5 mb-2 md:mx-4 mx-3 first:mt-4 last:mb-4 md:text-base text-sm antialiased font-sans"
+    class="relative animate__animated animate__faster rounded-full last-of-type:rounded-bl-none duration-700 transition-all bg-white dark:bg-slate-800 text-slate-900 dark:text-white ring-1 ring-slate-900/5 shadow w-fit !leading-5 mb-2 md:mx-4 mx-3 first:mt-4 last:mb-4 md:text-base text-sm antialiased font-sans"
     :class="
       type == 'loading'
         ? 'md:py-3 py-2 md:px-6 px-4 animate__fadeInUp'
@@ -16,7 +16,7 @@
     >
   </span>
   <div
-    class="stikernye text-right w-fit animate__animated animate__faster animate__fadeIn"
+    class="stikernye text-right w-fit animate__animated animate__slow animate__fadeIn"
     v-else
   >
     <nuxt-img
