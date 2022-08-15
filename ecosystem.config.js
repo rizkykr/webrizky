4739b8d39311aad201f13d028bc8ef31ad3910f4
Rizky Kurniawan Ritonga <rizkykurniawanritonga@gmail.com>
2022-08-16 05:22:11 +0700

    ekosistm

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "Web Rizky",
-      exec_mode: "fork",
-      instances: "1",
+      exec_mode: "cluster",
+      instances: "max",
       script: "./.output/server/index.mjs",
       args: "start",
     },
