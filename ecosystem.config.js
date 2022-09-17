1082aca4bc06e27ea6f712cb46b6a8aa48b8c9f0
Rizky Kurniawan Ritonga <rizkykurniawanritonga@gmail.com>
2022-09-18 01:49:33 +0700

    perbaiki ecosystem pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "Web Rizky",
+      name: "Web Rizky:3020",
       exec_mode: "fork",
       instances: "1",
       script: "./.output/server/index.mjs",
